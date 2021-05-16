61126b3f7bc442c4f6b8d0754fecfcc2159c5ebe
DominiquevdLinden <vdlindendominique33@gmail.com>
2021-05-16 13:01:06 +0200

    image and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -9,11 +7,18 @@ function App() {
         <nav id="navbar" className="sticky">
           <a>Contact</a>
           <a>Projects</a>
-          <a href="#welcome-section">Welcome</a>
+          <a>Welcome</a>
         </nav>
-        <section id="welcome-section">
-          <h1>Welcome!</h1>
-        </section>
+        <div className="welcome">
+          <section id="welcome-section">
+            <h1>Dominique van der Linden</h1>
+            <p>
+              Hi, I'm a developer mainly based on: Java/typescript, react and
+              currently working on Python!
+            </p>
+            <img src="https://avatars.githubusercontent.com/u/38658422?v=4"></img>
+          </section>
+        </div>
         <section id="projects">
           <div className="project-tile">
             <h1>Projects</h1>
