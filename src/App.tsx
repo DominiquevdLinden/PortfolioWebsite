fb68d14a74cb302d71c5f94f0859a968545eab86
DominiquevdLinden <vdlindendominique33@gmail.com>
2021-05-16 12:13:44 +0200

    freecodecamp compliant

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,32 @@ function App() {
   return (
     <div>
       <main id="main">
-        <nav id="navbar">
+        <nav id="navbar" className="sticky">
           <a>Contact</a>
           <a>Projects</a>
-          <a>Welcome</a>
+          <a href="#welcome-section">Welcome</a>
         </nav>
+        <section id="welcome-section">
+          <h1>Welcome!</h1>
+        </section>
+        <section id="projects">
+          <div className="project-tile">
+            <h1>Projects</h1>
+            <a target="_blank" href="google.com">
+              to Project
+            </a>
+          </div>
+        </section>
+        <section id="contact">
+          <h1>Contacts</h1>
+          <a
+            id="profile-link"
+            target="_blank"
+            href="https://github.com/DominiquevdLinden"
+          >
+            Github
+          </a>
+        </section>
       </main>
     </div>
   );
